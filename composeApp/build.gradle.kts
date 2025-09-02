8791f2f5c4282a411f1c4fc91d8e010ad376a0c7
Camila-Choque <m.choque@alumno.um.edu.ar>
2025-09-02 17:55:56 -0300

    Tab

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -65,6 +65,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(compose.materialIconsExtended)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.voyager.navigator)
